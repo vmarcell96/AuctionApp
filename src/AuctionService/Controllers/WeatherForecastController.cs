015248e200f6cde80092cda8073f2f770048d92d
Marcell Vajda <89020177+vmarcell96@users.noreply.github.com>
2025-12-27 18:29:13 +0100

    modify project dotnet version to 8 - lts

--- src/AuctionService/Controllers/WeatherForecastController.cs
+++ src/AuctionService/Controllers/WeatherForecastController.cs
@@ -6,10 +6,17 @@ namespace AuctionService.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries =
-    [
+    private static readonly string[] Summaries = new[]
+    {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    ];
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
