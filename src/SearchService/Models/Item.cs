8f5dc96729c2ce7ef00c186e31e82d397c5e7f17
Marcell Vajda <89020177+vmarcell96@users.noreply.github.com>
2026-01-06 16:08:29 +0100

    Init mongo db, Add seed data

---
+++ src/SearchService/Models/Item.cs
@@ -0,0 +1,22 @@
+using MongoDB.Entities;
+
+namespace SearchService.Models;
+
+//will behave like an AuctionDto
+public class Item : Entity
+{
+    public int ReservePrice { get; set; }
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public int SoldAmount { get; set; }
+    public int CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime AuctionEnd { get; set; }
+    public string AuctionStatus { get; set; }
+    public string Category { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Condition { get; set; }
+    public string ImageName { get; set; }
+}
