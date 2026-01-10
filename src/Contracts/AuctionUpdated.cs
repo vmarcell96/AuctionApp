160f891a3fb0d8fa2a967203547f5fbb81406dd4
Marcell Vajda <89020177+vmarcell96@users.noreply.github.com>
2026-01-10 13:50:31 +0100

    Add contracts

---
+++ src/Contracts/AuctionUpdated.cs
@@ -0,0 +1,10 @@
+namespace Contracts;
+
+public class AuctionUpdated
+{
+    public string Id { get; set; }
+    public string Category { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Condition { get; set; }
+}
